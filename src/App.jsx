89c57190e396134b1307e9aac757d5e8c115d06d
Bikash Mishra <mishrabikash202@gmail.com>
2024-01-26 18:38:54 +0530

    Store Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
 import './App.css'
-import conf from './conf/conf'
-import authservice from './appwrite/auth';
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import authservice from './appwrite/auth.js'
+import {Header,Footer} from './Component/index'
+import {login,logout} from './store/authSlice'
 
 function App() {
 
-  return (
-    <>
-     <h1 className=''>runnign</h1>
-    </>
-  )
+  const [loader, setLoder]=useState(true)
+  const Dispatch= useDispatch()
+
+
+  useEffect (()=>{
+    authservice.getCurrentUser().then((userData)=>{
+       if(userData){
+        Dispatch(login({userData}))
+       }
+       else{
+        logout()
+       }
+    })
+    .finally(()=>{
+       setLoder(false)
+    })
+  },[])
+
+  return !loader ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        TODO:  
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : <div>not loading</div>
 }
 
 export default App
